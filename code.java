6d878cefae91d9a6c5a971fa33c130cf6973d050
Maciej Wojdak <48714815+maciej99@users.noreply.github.com>
2022-09-03 16:27:16 +0200

    Update code.java

--- code.java
+++ code.java
@@ -13,23 +13,23 @@ public class Wall implements Structure {
   private List blocks;
 
   public Optional findBlockByColor(String color){
- 	  int size = blocks.size();
-	  Block block;
+ 	int size = blocks.size();
+	Block block;
    	int i = -1;
-	  while(++i<size){
+	while(++i<size){
   		block = blocks.get(i);
   		String c = block.getColor();
 	  	// gdy znajdziemy zadany kolor zwróć ten block, zrób type-casting na Optional
 	  	if(c.equals(color)) return (Optional) block; 
-	  }
+	}
   	// jesli pętla wykona się do końca nie znajdując żadnego elementu 
   	// o zadanym kolorze, zwróć null
   	return null;
   }
 
   public List findBlocksByMaterial(String material){
-	  // utwórz nową listę
-  	List<Block> matching = new List<>();
+	// utwórz nową listę
+  	List<Block> matching = new ArrayList<>();
   	int size = blocks.size();
   	Block block;
   	int i = -1;
@@ -38,13 +38,13 @@ public class Wall implements Structure {
 	  	String m = block.getMaterial();
 	  	// gdy znajdziemy zadany material dodaj do listy matching
 	  	if(m.equals(material)) matching.add(block); 
-	  }
-	  // zwróć listę matching
-	  return matching; 
+	}
+	// zwróć listę matching
+	return matching; 
   }
 
   public int count(){
-	  return blocks.size();
+	return blocks.size();
   }
 }
 
